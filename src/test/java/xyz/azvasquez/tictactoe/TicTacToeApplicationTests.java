2051f8d3ac6119c61ab2f82731e245d0216486f4
AZ Vasquez <a16zebra@gmail.com>
2026-08-05 20:43:02 -0400

    Use Java

---
+++ src/test/java/xyz/azvasquez/tictactoe/TicTacToeApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.azvasquez.tictactoe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicTacToeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
